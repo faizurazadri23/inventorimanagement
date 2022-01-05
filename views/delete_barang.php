d55be9c9cc1470fa6ba3b453103f631d777e2452
Faizura Zadri <faizura@explorindo.com>
2022-01-06 00:17:07 +0700

    delete data barang

--- views/delete_barang.php
+++ views/delete_barang.php
@@ -4,12 +4,12 @@
 	$crud = new Barang();
 
 	if(isset($_GET['id'])){
-		$id_barang = $_GET['id']; //menampung nim
+		$id_barang = $_GET['id'];
 
 		$result = $crud->deleteData($id_barang);
 
 		if($result){
-			//alert dan redirect ke index.php
+			
 			echo "<script>alert('Barang Berhasil dihapus.');window.location='index.php?page=barang';</script>";
 		}else{
 			echo "<script>alert('data gagal dihapus.');</script>";
