b24b443da27fb84e500d99cf900f253b1e4f85f2
Faizura Zadri <faizura@explorindo.com>
2022-01-23 17:36:36 +0700

    add Login

---
+++ app/controllers/Login.php
@@ -0,0 +1,14 @@
+<?php
+
+class Login extends Controller{
+
+
+    public function index()
+    {
+       
+        $this->view('login/index');
+       
+    }
+}
+
+?>
---
+++ app/views/login/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Inventory Management Login</title>
+        <link rel="stylesheet" href="../public/css/style.css">
+    </head>
+   
+    <body>
+        <div class="container">
+          <h1>LOGIN</h1>
+            <form>
+                <label>Nama Pengguna</label><br>
+                <input type="text"><br>
+                <label>Kata Sandi</label><br>
+                <input type="password"><br>
+                <button>MASUK</button>
+            </form>
+        </div>     
+    </body>
+</html>
